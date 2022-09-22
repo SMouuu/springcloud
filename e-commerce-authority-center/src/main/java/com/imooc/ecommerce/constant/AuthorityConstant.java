db80707efe0f35c48d625a845ac536c7a70a2f69
SMou <1197864339@qq.com>
2022-09-22 17:23:30 +0800

    生成RAS公钥和私钥

---
+++ e-commerce-authority-center/src/main/java/com/imooc/ecommerce/constant/AuthorityConstant.java
@@ -0,0 +1,34 @@
+package com.imooc.ecommerce.constant;
+
+
+/**
+ * 授权需要使用的一些常量信息
+ */
+public class AuthorityConstant {
+
+    /**
+     * RSA 私钥 除了授权中心以外不暴露给任何客户端
+     */
+    public static final String PRIVATE_KEY="MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCBbjM9WGjun4Ln+" +
+            "iiL6ikDRfenCGZvuVlmbqKN3U1gAqQEE30jx7oydvrEmGUBM7+V3Ks90cKmopqrj696MQzcDUzg42ZK9wG6mSxnfne3aH26o4" +
+            "Zg52YGW4lUYr4ilPNQHyvCc59kAB/WojDmwzmESV0+rTzc7FVFGAoCF+oG+O5oweX2dR3beQWsuE+3H5CwaEb60tExyP0iGFpl" +
+            "DwOqW4Zp8A7Ecn8izAGtj0PjAhKBSm3TCJnn0VRcqOPE/KmYHjcNZKtR+HTAhKC641UNghWrNbMKmsjK9XaOTOB2eVxKKGJLmzk" +
+            "96CFs7UI74l/XJqWGxtkrqqvrK3l2V1OlAgMBAAECggEAcK5gLagqspRsg0lmw2i5DAB9+w11IQ9cpbDsFFX2Zv/liybsa5oWoJ" +
+            "i0+TMzvDgxM2dKLifQLwKhZXtSai8J4woxRBi1lc410x5HalLQJvzxeOuksfjC+gX3KWBN6A2FRtvcXQ4MViNFlLbfnAeaq3Czn" +
+            "T11H2EhW2XyUKBBPo99CGueQQ5/GbIgqPeRT0FLe0G1Xk+OGSQ5AKZHU98fXAVy9QB6RaWovmSrBj3orZvPnjB6KkhaWfVWpzs/r" +
+            "iUrUBowmHszPeSO6NZT/qMq2KW57C/13QG/Fl948xZ1P0BTzc13zWv0D5tbMUaUQztuLNtwh5coOtnb90mcqTMgAQKBgQC393wgC6" +
+            "vINq8Tyf8rT0nk/lwMbr4PxYwD7lJ4y7EtR3x+fNUb1OMj59ww99lFM9CKNns2bpvwbFnB0gnkmsmBh9eU15C21r9hjNAqt+DNZ+KS" +
+            "KxmDoQhuUqTgJNU4lLvXQJbNo7dEtaL17JrF0S5LvLzxmSyyUXr7fyty9E2XwQKBgQC0HBeCzymIAL7FZf0UlNWxvcMSoeIESo8Ez2o" +
+            "+Fvf0RbQIq0JdTL4ZKoI5CtAPJOhR5ypsQKmEt0TQlSphVIidakZX5YPJJtXy1jZFGeDj180WlnbGHIIAxNk0jUr3LuN7f3DR19QnqoQm" +
+            "/mA+oldkeEUaJGogKrlsAG9aRfOU5QKBgErXjlHjLaGoTZW5aewOt7tHl98e9oeoEIxQ2pB2HcCUApva4UAPXdDvtO7fBi9XIxH3lCzS" +
+            "1vH6wpFNh+TO7AecYiIViLQ5rWZcSRv37qbiCpT4n8qjKrDnQlhF49C/AaRSjIdggWCcnI18EoND1JfPUvbfIAXzjBrg46ZCqYaBAoGAa" +
+            "p6mnF0ebNBkHl79ro1c41c+f20tavToLSTL8+2M4yHQLcGUC78+WY98f2ATQqPKk5GQ33x33YTQS9nBuNRd+zHKaoYHubAvvw1oGwfyxW" +
+            "4ejlyL+0MtAl+FZUKvOhybtaP/mXl2sfSTPrIfL3TS3zsDTTJ5UIl6MqEfSgVW6LECgYBpyBs9kcrfGlaMMh/vTBbj+gEbQTrhRS1z/Ncb" +
+            "yQ7MAW604C6TubWmcqsE/1oDZis4JZCfN57TpTrbNBaltnXWmv+JNEZxPL7uR9zzkuTSG66Cw2YO2poe00Byzu6gw0tN/Nwe3CgH1G6GwB" +
+            "ZWVu+XsThsObQ19MVUW7WBTfmlHg==";
+    /**
+     * 默认的token超时时间，1天
+     */
+    public static final Integer DEFAULT_EXPIRE_DAY = 1;
+
+}
---
+++ e-commerce-common/src/main/java/com/imooc/ecommerce/constant/CommonConstant.java
@@ -0,0 +1,28 @@
+package com.imooc.ecommerce.constant;
+
+/**
+ * 通用模块常量定义
+ */
+public class CommonConstant {
+
+    /**
+     * RSA公钥
+     */
+    public static final String PUBLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgW4zPVho7p+C5" +
+            "/ooi+opA0X3pwhmb7lZZm6ijd1NYAKkBBN9I8e6Mnb6xJhlATO/ldyrPdHCpqKaq4+vejEM3A1M4ONmSvcBupksZ3" +
+            "53t2h9uqOGYOdmBluJVGK+IpTzUB8rwnOfZAAf1qIw5sM5hEldPq083OxVRRgKAhfqBvjuaMHl9nUd23kFrLhPtx+Qs" +
+            "GhG+tLRMcj9IhhaZQ8DqluGafAOxHJ/IswBrY9D4wISgUpt0wiZ59FUXKjjxPypmB43DWSrUfh0wISguuNVDYIVqzWz" +
+            "CprIyvV2jkzgdnlcSihiS5s5PeghbO1CO+Jf1yalhsbZK6qr6yt5dldTpQIDAQAB";
+
+    /**
+     * JWT 中存储用户信息的key
+     */
+    public static final String JWT_USER_INFO_KEY="e-commerce-user";
+
+    /**
+     * 授权中心的service id
+     */
+    public static final String AUTHORITY_CENTER_SERVICE_ID="e-commerce-authority-center";
+
+
+}
