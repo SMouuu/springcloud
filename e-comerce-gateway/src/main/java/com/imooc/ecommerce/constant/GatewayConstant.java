e9e2c0831a5d21a36f71ddcd07903b41fd9b79b9
SMou <1197864339@qq.com>
2022-09-23 17:29:19 +0800

    1.新增登录、注册、鉴权全局过滤器

--- e-comerce-gateway/src/main/java/com/imooc/ecommerce/constant/GatewayConstant.java
+++ e-comerce-gateway/src/main/java/com/imooc/ecommerce/constant/GatewayConstant.java
@@ -10,11 +10,11 @@ public class GatewayConstant {
     //注册uri
     public static final String REGISTER_URI = "/e-comerce/register";
 
-    //去授权中心登录拿token的uri格式化接口
-    public static final String AUTHORITY_CENTER_TOKEN_URI_FORMAT =
+    /** 去授权中心拿到登录 token 的 uri 格式化接口 */
+    public static final String AUTHORITY_CENTER_TOKEN_URL_FORMAT =
             "http://%s:%s/ecommerce-authority-center/authority/token";
 
-    //去授权中心注册拿token的uri格式化接口
-    public static  final String AUTHORITY_CENTER_REGISTER_URI_FORMAT =
+    /** 去授权中心注册并拿到 token 的 uri 格式化接口 */
+    public static final String AUTHORITY_CENTER_REGISTER_URL_FORMAT =
             "http://%s:%s/ecommerce-authority-center/authority/register";
 }
