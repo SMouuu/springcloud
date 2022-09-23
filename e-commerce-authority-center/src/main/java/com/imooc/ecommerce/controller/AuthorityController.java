efa691edbac08a102f36df2ef5c9f443901597e1
SMou <1197864339@qq.com>
2022-09-23 10:44:52 +0800

    1.完成注册服务的controller

---
+++ e-commerce-authority-center/src/main/java/com/imooc/ecommerce/controller/AuthorityController.java
@@ -0,0 +1,65 @@
+package com.imooc.ecommerce.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.ecommerce.annotation.IgnoreResponseAdvice;
+import com.imooc.ecommerce.service.IJWTService;
+import com.imooc.ecommerce.vo.JwtToken;
+import com.imooc.ecommerce.vo.UsernameAndPassword;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 对外暴露授权服务接口
+ */
+@Slf4j
+@RestController
+@RequestMapping("/authority")
+public class AuthorityController {
+
+    private IJWTService ijwtService;
+
+    public AuthorityController(IJWTService ijwtService) {
+        this.ijwtService = ijwtService;
+    }
+
+    /**
+     * 从授权中心获取token(登录),且返回信息中没有统一的响应包装
+     */
+    @IgnoreResponseAdvice
+    @PostMapping("/token")
+    public JwtToken token(@RequestBody UsernameAndPassword usernameAndPassword) throws Exception{
+
+        log.info("request to get token with param: [{}]", JSON.toJSONString(usernameAndPassword));
+        return new JwtToken(ijwtService.generateToken(
+                usernameAndPassword.getUsername(),usernameAndPassword.getPassword()
+        ));
+    }
+
+
+    /**
+     * 注册用户并返回用户token，即通过授权中心创建用户
+     */
+    @IgnoreResponseAdvice
+    @PostMapping("/register")
+    public JwtToken register(@RequestBody UsernameAndPassword usernameAndPassword) throws Exception{
+
+        log.info("register user with param: [{}]",JSON.toJSONString(usernameAndPassword));
+        return new JwtToken(ijwtService.registerUserAndGenerateToken(usernameAndPassword));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
